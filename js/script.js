e23386b3a7012efcd687d505fcd5b488718409e3
Negar-Arianmehr <negar.arianmehr89@gmail.com>
2021-02-10 22:34:38 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -38,24 +38,40 @@ $(document).ready(function(){
 
 
 $('.owl-carousel').owlCarousel({
-    loop:true,
+    // loop:true,
     margin:10,
-    autoplay: true,
+    // autoplay: true,
     responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
-        }
+    autoplay: true, //set to false to turn off autoplay and only use nav
+        autoplayHoverPause: true, //set to false to prevent pausing on hover
+        loop: true, //set to false to stop carousel after all slides shown
+        autoplayTimeout: 8000, //time between transitions
+        smartSpeed: 1200, //transition speed
+        navSpeed: 1000, //transition speed when using dots/buttons
+        responsive : { //set number of items shown per screen width
+            0 : {
+                items: 1 //0px width and up display 1 item
+            },
+            768 : {
+                items: 2 //576px width and up display 2 items
+            },
+            992 : {
+                items: 3 //768px width and up display 3 items
+            }
+    // responsive:{
+    //     0:{
+    //         items:1,
+    //         nav:true
+    //     },
+    //     600:{
+    //         items:2,
+    //         nav:false
+    //     },
+    //     1000:{
+    //         items:5,
+    //         nav:true,
+    //         loop:false
+    //     }
     }
 })
 
